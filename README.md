124bac7983c49254d8b44307c4c254c802fed07a
mecaneer23 <mecaneer23@gmail.com>
2022-03-03 12:19:52 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bash Snake Game
+
+This is currently a work in progress - don't expect it to work yet.
--- snake.sh
+++ snake.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
+# recreate the snake game in bash
 
-BOARD_SIZE=10
-SNAKE_CHAR='■ '
-FOOD_CHAR='X '
-BG_CHAR='. '
-input=''
-snake_x=3
-snake_y=3
-clear
+ROWS=20
+COLS=50
+CHAR_SNAKE="#"
+CHAR_FOOD="*"
+CHAR_BG="."
+UP=(0, -1)
+DOWN=(0, 1)
+LEFT=(-1, 0)
+RIGHT=(1, 0)
 
-draw() {
-    clear
-    for i in $(seq 1 $BOARD_SIZE); do # columns
-        for j in $(seq 1 $BOARD_SIZE); do # rows
-            if [[ $j -eq $snake_x && $i -eq $snake_y ]]; then
-                echo -ne "$SNAKE_CHAR"
-            elif [[ $j -eq $food_x && $i -eq $food_y ]]; then
-                echo -ne "$FOOD_CHAR"
-            else
-                echo -ne "$BG_CHAR"
-            fi
-        done
-        echo -ne "\n"
-    done
-}
+# draw() {
+#     clear
+#     for i in $(seq 1 $BOARD_SIZE); do # columns
+#         for j in $(seq 1 $BOARD_SIZE); do # rows
+#             if [[ $j -eq $snake_x && $i -eq $snake_y ]]; then
+#                 echo -ne "$SNAKE_CHAR"
+#             elif [[ $j -eq $food_x && $i -eq $food_y ]]; then
+#                 echo -ne "$FOOD_CHAR"
+#             else
+#                 echo -ne "$BG_CHAR"
+#             fi
+#         done
+#         echo -ne "\n"
+#     done
+# }
 
 
 
-while true; do 
+while true; do
     draw
     # read -s -n1 input
     # get input asynchronously
     # if input is not valid, draw the board again
     # if input is valid, move the snake in the direction of the input
-    
+
     read -s -n1 input &
     case $input in
         w)
